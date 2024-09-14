554ee3dc3f4bba3bb1b30b3d086932c0079b015a
smoorke <smoorke@hotmail.com>
2024-09-14 15:28:40 +0200

    Update frmMain.Designer.vb

--- frmMain.Designer.vb
+++ frmMain.Designer.vb
@@ -141,7 +141,7 @@ Partial Class frmMain
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(34, 32)
+        Me.ClientSize = New System.Drawing.Size(0, 0)
         Me.ControlBox = False
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
