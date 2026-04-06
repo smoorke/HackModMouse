da8dc7d451b66479dd23c0d9fb5f2945ecf8010f
smoorke <smoorke@hotmail.com>
2026-04-06 21:35:09 +0200

    1.2.0 release

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -9,7 +9,7 @@ Imports System.Runtime.InteropServices
 ' Review the values of the assembly attributes
 
 <Assembly: AssemblyTitle("HackMod Mouse Enabler")>
-<Assembly: AssemblyDescription("Enable MouseCursor in hackmud")>
+<Assembly: AssemblyDescription("Enable MouseCursor in HackMud")>
 <Assembly: AssemblyCompany("")>
 <Assembly: AssemblyProduct("HackModMouse")>
 <Assembly: AssemblyCopyright("Copyright ©  2026")>
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("1.1.0.0")>
-<Assembly: AssemblyFileVersion("1.1.0.0")>
+<Assembly: AssemblyVersion("1.2.0.0")>
+<Assembly: AssemblyFileVersion("1.2.0.0")>
