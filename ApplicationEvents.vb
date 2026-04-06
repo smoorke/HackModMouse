c697320391d4df5027d1ab904f4c7235b66234e2
smoorke <smoorke@hotmail.com>
2026-04-06 20:58:05 +0200

    show menu on 2nd run
    so that incase you don't have access to primary monitor you can still open the menu

--- ApplicationEvents.vb
+++ ApplicationEvents.vb
@@ -1,4 +1,5 @@
-﻿Imports Microsoft.VisualBasic.ApplicationServices
+﻿Imports System.Runtime.InteropServices
+Imports Microsoft.VisualBasic.ApplicationServices
 
 Namespace My
     ' The following events are available for MyApplication:
@@ -9,8 +10,31 @@ Namespace My
     ' NetworkAvailabilityChanged: Raised when the network connection is connected or disconnected.
     Partial Friend Class MyApplication
         Private Sub MyApplication_StartupNextInstance(sender As Object, e As StartupNextInstanceEventArgs) Handles Me.StartupNextInstance
-            If IsIconic(hackMudHandle) Then SendMessage(hackMudHandle, WM_SYSCOMMAND, SC_RESTORE, 0)
-            SetForegroundWindow(hackMudHandle)
+            If hackMudHandle <> IntPtr.Zero Then
+                If IsIconic(hackMudHandle) Then SendMessage(hackMudHandle, WM_SYSCOMMAND, SC_RESTORE, 0)
+
+                Dim foreThread As Integer = GetWindowThreadProcessId(GetForegroundWindow(), Nothing)
+                Dim currentThread As Integer = GetCurrentThreadId()
+
+                Try
+                    AttachThreadInput(currentThread, foreThread, True)
+                    SetForegroundWindow(hackMudHandle) 'w/o attaching to the fgw (explorer) this may fail.
+                    SetActiveWindow(hackMudHandle)
+                Finally
+                    AttachThreadInput(currentThread, foreThread, False)
+                End Try
+
+            End If
+
+            e.BringToForeground = False
+
+            cmsTray.Tag = ToolStripDropDownDirection.Default
+            cmsTray.Show(Cursor.Position)
+
         End Sub
+        <DllImport("kernel32.dll")>
+        Private Shared Function GetCurrentThreadId() As Integer : End Function
+        <DllImport("user32.dll")>
+        Public Shared Function SetActiveWindow(hWnd As IntPtr) As IntPtr : End Function
     End Class
 End Namespace
